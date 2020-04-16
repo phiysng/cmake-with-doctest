ae270a2e4f64763f2b40e4d45c3d2410b003a0ac
Wu Yuanshou <wuyuanshou@protonmail.com>
2020-04-16 09:46:22 +0800

    minimum cmake test example. test with quick-sort.

---
+++ tests/test2.cpp
@@ -0,0 +1,22 @@
+//
+// Created by phiysng on 2020/4/16.
+//
+
+#include "doctest.h"
+#include <unistd.h>
+#include <vector>
+#include <src/quick_sort.h>
+
+TEST_CASE ("sleep a second") {
+            CHECK(sleep(1) == 0);
+}
+
+TEST_CASE ("sort reversed vector") {
+    typedef std::vector<int> VecInt;
+    VecInt v{9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+    quick_sort(v, 0, v.size());
+            CHECK_EQ(v, VecInt{1, 2, 3, 4, 5, 6, 7, 8, 9});
+            CHECK_EQ(VecInt{0}, VecInt{});
+
+}
